ebd17da3c1e7b95240d3da3e01ad09eeba4115e9
TurtlePaw <turtlesimsyt@gmail.com>
2021-06-15 19:54:37 -0400

    ?

--- models/message.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const CatSchema = new mongoose.Schema({
-    message: String,
-    reply: String
-});
-
-const CatModel = module.exports = mongoose.model('message', CatSchema);
