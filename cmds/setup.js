a8a65ca2649c56b8c4d697bb192691344fb70c11
TurtlePaw <turtlesimsyt@gmail.com>
2021-06-15 19:54:47 -0400

    Added setup and help

---
+++ cmds/setup.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const wait = require('util').promisify(setTimeout);
+
+module.exports = {
+  name: "setup",
+  description: "Setup the server!",
+  async execute(message, Member, args) {
+      if(!message.guild.me.permissions.has('MANAGE_CHANNELS')){
+          return message.reply('I don\'t have permissions')
+      }
+    if(message.guild.me.permissions.has('MANAGE_CHANNELS') && message.member.permissions.has('ADMINISTRATOR')){
+    const ch = await message.guild.channels.create('bp', { //Create a channel
+        type: 'text' //Make sure the channel is a text channel
+    });
+    const yesembed = new Discord.MessageEmbed()
+    .setTitle('Created!')
+    .addField('Channel:', `▶ ${ch}`, true)
+    .addField('ID:', `▶ ${ch.id}`, true)
+    .setColor('GREEN')
+    message.channel.send(yesembed)
+}
+
+  }
+}
