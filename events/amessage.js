2f898ceb37dda8c161114f13a975d01f0f3435c3
TurtlePaw <turtlesimsyt@gmail.com>
2021-06-15 19:26:59 -0400

    Added basic features ✅

---
+++ events/amessage.js
@@ -0,0 +1,37 @@
+const msgg = require('../models/message')
+
+module.exports = {
+	name: 'message',
+	async execute(message, client, args) {
+        try{
+        if(message.channel.name != 'bp'){
+            return
+        }
+        if(message.author.id === client.user.id){
+            return
+        }
+            let cmdsa = await msgg.findOne({
+                message: message.content
+            });
+            if(cmdsa){
+                message.reply({ content: `${cmdsa.reply}` })
+            } else {
+                const lastdm = [
+                    `Wth?`,
+                    `Wdym?`,
+                    `What do you mean?`,
+                    `What?`,
+                    `?`,
+                    `??`
+                ]
+                message.reply({ content: `${lastdm[Math.floor(Math.random() * lastdm.length)]}` })
+            }
+        } catch (error){
+            console.log('Error!', error)
+        }
+		/*
+            message: String,
+    reply: string
+    */
+	},
+};
