37faeb2d3401efb81061bd43041265ae5f33df85
TurtlePaw <turtlesimsyt@gmail.com>
2021-06-18 19:54:57 -0400

    Added return if embed

--- events/message.js
+++ events/message.js
@@ -15,10 +15,16 @@ module.exports = {
         let cmdsa = await msgg.findOne({
             message: message.content
         });
-        if(cmdsa?.message === message.content){
-            return
+        // if(cmdsa?.message === message.content){
+        //     return
+        // }
+        if(message.embeds.length > 0){
+            return console.log('Embed:', message.embeds)
         }
         message.channel.awaitMessages(filter, { max: 1, time: 50000, errors: ['time'] }).then(async collected => {
+            if(collected.first().embeds.length > 0){
+                return console.log('Embed:', message.embeds)
+            }
             cmdsa = new msgg({
                 user: message.author.id,
                 message: message.content,
