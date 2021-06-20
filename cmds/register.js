ff4f622bd88665aa6851a0b73bd3e13fbd0a8595
TurtlePaw <turtlesimsyt@gmail.com>
2021-06-20 16:34:04 -0400

    Add /info

--- cmds/register.js
+++ cmds/register.js
@@ -29,9 +29,8 @@ module.exports = {
                 required: false,
             }],
         };
-
-        const command = await client.application?.commands.create(data);
-        //const command = await client.guilds.cache.get('842575277249921074')?.commands.create(data);
+        //const command = await client.application?.commands.create(data);
+        const command = await client.guilds.cache.get('842575277249921074')?.commands.create(data);
         //await command.setPermissions(permissions);
         console.log(command);
     }
